45cfe557044001fdb1afa3688200f81061601331
hjjvandam <hvandam@bnl.gov>
2020-04-05 13:57:57 -0400

    Replace hardwired paths with env variables.

--- ProcessingScripts/smiles_dock.sh
+++ ProcessingScripts/smiles_dock.sh
@@ -34,15 +34,15 @@ do
   fi
   cd $id
   echo "$smiles" | obabel -h --gen3d -ismi -omol2 > $id.mol2
-  pythonsh ../Utilities24/prepare_ligand4.py -l $id.mol2  -o $id.pdbqt
+  pythonsh $AUTODOCKTOOLS_UTIL/prepare_ligand4.py -l $id.mol2  -o $id.pdbqt
   if [[ $# -eq 4 ]]
   then
-    pythonsh ../Utilities24/prepare_gpf4.py  -l $id.pdbqt -r $1.pdbqt -p npts="$4" -p gridcenter="$3" -o $id.gpf
-    pythonsh ../Utilities24/prepare_dpf42.py -l $id.pdbqt -r $1.pdbqt -p ga_run=20 -o $id.dpf
+    pythonsh $AUTODOCKTOOLS_UTIL/prepare_gpf4.py  -l $id.pdbqt -r $1.pdbqt -p npts="$4" -p gridcenter="$3" -o $id.gpf
+    pythonsh $AUTODOCKTOOLS_UTIL/prepare_dpf42.py -l $id.pdbqt -r $1.pdbqt -p ga_run=20 -o $id.dpf
   elif [[ $# -eq 5 ]]
   then
-    pythonsh ../Utilities24/prepare_gpf4.py  -l $id.pdbqt -r $1.pdbqt -x $5.pdb -p npts="$4" -p gridcenter="$3" -o $id.gpf
-    pythonsh ../Utilities24/prepare_dpf42.py -l $id.pdbqt -r $1.pdbqt -x $5.pdb -p ga_run=20 -o $id.dpf
+    pythonsh $AUTODOCKTOOLS_UTIL/prepare_gpf4.py  -l $id.pdbqt -r $1.pdbqt -x $5.pdb -p npts="$4" -p gridcenter="$3" -o $id.gpf
+    pythonsh $AUTODOCKTOOLS_UTIL/prepare_dpf42.py -l $id.pdbqt -r $1.pdbqt -x $5.pdb -p ga_run=20 -o $id.dpf
   else
     echo "invalid number of arguments $#"
     echo "should be 4 or 5"
