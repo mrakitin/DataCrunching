960d085798ca01446cc02fe644eec4d5029b6ecf
Xiaohui Qu <xiaqu@bnl.gov>
2020-04-08 12:00:34 -0400

    delete temporary file after job is done

--- ProcessingScripts/scripts/run_sub_par.sh
+++ ProcessingScripts/scripts/run_sub_par.sh
@@ -7,6 +7,9 @@ parallel -a $1 --timeout=3600 --colsep '\s+' -j ${SLURM_CPUS_ON_NODE} "
     cp ../data/{3}.pdb .;
     echo {1} {2} > {2}_smi.can; 
     ../scripts/run_all_crunch.sh {2}_smi.can {3} {4} {5} &> {2}_stdout.txt; 
+    echo delete all temporary files;
+    rm *.pdb *.pdbqt;
+    rm */*.dpf */*.fld */*.glg */*.gpf */*.map */*.mol2 */*.pdbqt */*.xyz
     "
 
 
